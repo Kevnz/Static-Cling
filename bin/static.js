c0533e238fa52e4de907fed3cc796f835d3af55a
Kevin Isom <kevin.isom@gmail.com>
2012-03-28 12:58:55 +1300

    added default file option as well

--- bin/static.js
+++ bin/static.js
@@ -1,10 +1,10 @@
 #!/usr/bin/env node
-console.log('starting static file server');
 var path = require('path'),
 	argv = require('optimist').argv,
 	cling = require('../lib/static').cling,
 	port = 3000,
-	dir = '.';
+	dir = '.',
+	file = 'index.html';
 	
 if(argv.p){
 	port = argv.p;
@@ -12,10 +12,13 @@ if(argv.p){
 if(argv.d){
 	dir =  './'+ argv.d + '/';
 }
+if(argv.f){
+	file = argv.f;
+}
 path.exists(dir, function (exists) {
 	if(exists){
 		console.log('starting to serve files in ' +  dir + ' on port ' + port);
-		cling(port, dir);
+		cling(port, dir, file);
 	}else{
 		console.log('Directory does not exist');
 	}
