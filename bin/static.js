f2aad4fb5c5343cb80c2861da7792a880f59bb33
Kevin Isom <kevin.isom@gmail.com>
2015-12-26 16:57:33 +1300

    update to run on latest node versions

--- bin/static.js
+++ bin/static.js
@@ -1,11 +1,11 @@
 #!/usr/bin/env node
-var path = require('path'),
+var existsCheck = require('../utils/exists'),
 	argv = require('optimist').argv,
 	cling = require('../lib/static').cling,
 	port = 3000,
 	dir = '.',
 	file = 'index.html';
-	
+
 if(argv.p){
 	port = argv.p;
 }
@@ -15,7 +15,7 @@ if(argv.d){
 if(argv.f){
 	file = argv.f;
 }
-path.exists(dir, function (exists) {
+existsCheck(dir, function (exists) {
 	if (exists) {
 		console.log('starting to serve files in ' +  dir + ' on port ' + port);
 		cling({ port: port, root: dir, filename: file });
@@ -24,4 +24,4 @@ path.exists(dir, function (exists) {
 	}
 });
 
- 
+
--- test/static.test.js
+++ test/static.test.js
@@ -1,5 +1,10 @@
 var cling = require('../lib/static').cling,
-	port= 8083; 
+	port= 8083;
 console.log('starting server with port ' + port)
-cling({ port: port });
+cling({ port: port, root: __dirname, filename: 'sample.html' });
+
+setTimeout(function () {
+    console.log('shutting down');
+    process.exit(0);
+}, 20000);
 
